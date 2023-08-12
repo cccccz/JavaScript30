41c8b686f5fcb9f250e30c30a32f86e75472db1f
Zheng Chen <chenzheng0430@gmail.com>
2023-08-12 08:19:58 -0400

    scrub working

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -26,8 +26,19 @@ function skip(){
 
 function handleRangeUpdate(){
     video[this.name] = this.value;
-    console.log(this.name);
-    console.log(this.value);
+    // console.log(this.name);
+    // console.log(this.value);
+}
+
+function handleProgress(){
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+    const scrubTime = (e.offsetX/progress.offsetWidth)*video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
 }
 
 video.addEventListener('click',togglePlay);
@@ -36,7 +47,15 @@ toggle.addEventListener('click',togglePlay);
 video.addEventListener('play',updateButton);
 video.addEventListener('pause',updateButton);
 
+video.addEventListener('timeupdate',handleProgress);
+
 skipButtons.forEach(button=>button.addEventListener('click',skip));
 
 ranges.forEach(range=>range.addEventListener('change',handleRangeUpdate));
 ranges.forEach(range=>range.addEventListener('mousemove',handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click',scrub);
+progress.addEventListener('mousemove',(e)=>mousedown&& scrub(e));
+progress.addEventListener('mousedown',()=>mousedown=>true);
+progress.addEventListener('mouseup',()=>mousedown =false);
